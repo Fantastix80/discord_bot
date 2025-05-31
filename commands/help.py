931d5588d03eb584252de2441b17f794bbd5ff59
jean <jean.vanwittenberge@gmail.com>
2025-05-31 14:35:50 +0200

    Ajout d'une indication dans le menu d'aide

--- commands/help.py
+++ commands/help.py
@@ -15,7 +15,7 @@ class Help(commands.Cog):
         )
         embed.add_field(name="⛏️!minecraft", value="Affiche l’état du serveur Minecraft et les joueurs connectés.", inline=False)
         embed.add_field(name="🛠️ !command", value="Permet de créer ta propre commande personnalisée.", inline=False)
-        embed.add_field(name="🛠️ !say <message>", value="Permet d'envoyer un message sur le serveur minecraft.", inline=False)
+        embed.add_field(name="🛠️ !say <message>", value="Permet d'envoyer un message sur le serveur minecraft.\nSeulement utilisable dans le channel `minecraft` !", inline=False)
         embed.add_field(name="❔!help", value="Affiche ce menu d'aide.", inline=False)
         await ctx.reply(embed=embed)
 
